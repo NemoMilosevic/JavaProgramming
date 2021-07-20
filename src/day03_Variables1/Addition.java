94f7600d0528bc157477648939bdedfb2c41d6b3
Nemanja M <nemanja985@yahoo.com>
2021-07-20 12:24:07 -0400

    Test

---
+++ src/day03_Variables1/Addition.java
@@ -0,0 +1,17 @@
+package day03_Variables1;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+
+
+
+}
